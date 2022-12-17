5aa78f9764922171197a7152ca9387e1d5c70e86
cochranejwink <cochranejwink@yahoo.co.uk>
2022-12-17 16:43:42 +0000

    Create MenuBuilderA.java
    This is Menu Builder using ArrayList Jc 17/12/2022

---
+++ _01_04/MenuBuilderA.java
@@ -0,0 +1,44 @@
+package _01_04;
+
+import java.util.ArrayList; 
+/**
+ * JC 171222
+ */
+public class MenuBuilder {
+  public static void main(String[] args) {
+
+    // Create a variable called menuTitle of type String and assign it the value "My
+    // Dream Menu:".
+    String menuTitle = "My Dream Menu:";
+
+    // Print the menuTitle variable to the console.
+    System.out.println(menuTitle);
+
+    // Create a variable called menu of type ArrayList.
+    ArrayList<MenuItem> menu = new ArrayList<>();
+
+    // Create a variable called starter of type MenuItem and pass in the name of
+    // your favourite starter.
+    MenuItem starter = new MenuItem("Pakora");
+
+    // Add the starter variable to the ArrayList called menu.
+    menu.add(0, starter);
+
+    // Create a variable called mainCourse of type MenuItem and pass in the name of
+    // your favourite main course.
+    MenuItem main = new MenuItem( "Chicken Dhansak");
+
+    // Add the mainCourse variable to the ArrayList called menu.
+    menu.add(0, main);
+    
+    // Create a variable called dessert of type MenuItem and pass in the name of
+    // your favourite dessert.
+    MenuItem dessert = new MenuItem( "Chocolate Icecream");
+
+    // Add the dessert variable to the ArrayList called menu.
+    menu.add(0, dessert);
+
+    // Print the menu variable to the console.
+      System.out.println(menu);
+  }
+}
