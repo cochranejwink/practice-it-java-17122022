6c4c6ee29f4f8fb16ffb0c14d708d39ecbb3867f
cochranejwink <cochranejwink@yahoo.co.uk>
2022-12-17 13:28:18 +0000

    Update MenuBuilder.java
    This is my Menu HashMap tested in netbeans JC 17/12/2022

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -2,31 +2,37 @@ package _01_04;
 
 import java.util.ArrayList;
 
+import java.util.HashMap; 
+/**
+ * JC 171222
+ */
 public class MenuBuilder {
   public static void main(String[] args) {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
+    String menuTitle = "My Dream Menu:";
 
     // Print the menuTitle variable to the console.
+    System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
+    HashMap<String, String> menu = new HashMap<String, String>();
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
+    menu.put("Starter", "Pakora");
 
-    // Add the starter variable to the ArrayList called menu.
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
-
-    // Add the mainCourse variable to the ArrayList called menu.
+    menu.put("Main", "Chicken Dhansak");
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
-
-    // Add the dessert variable to the ArrayList called menu.
+    menu.put("Dessert", "Chocolate IceCream");
 
     // Print the menu variable to the console.
+      System.out.println(menu);
   }
 }
